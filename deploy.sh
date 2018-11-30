5593f25a66e2c316438a60427d973869705c8fdc
Daniel Weibel <danielmweibel@gmail.com>
2018-11-30 15:55:05 -0500

    Rename Test to Example

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sam package --template-file template.yml --output-template-file package.yml --s3-bucket quantumsense-sam
-sam deploy --template-file package.yml --capabilities CAPABILITY_IAM --stack-name lambda-eks-test
+sam deploy --template-file package.yml --capabilities CAPABILITY_IAM --stack-name lambda-eks-example
--- local.sh
+++ local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sam local invoke --no-event LambdaEksTestFunction
+sam local invoke --no-event LambdaEksExampleFunction
